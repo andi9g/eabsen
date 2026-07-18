a5176f94a3c98c26afdfab535e9611f2ca644bf9
andi9g <andirizky.bayuputra@gmail.com>
2026-07-18 18:16:22 +0700

    auto commit

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -26,21 +26,31 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        // 1. Cek apakah request datang dari proxy/cloudflare dengan HTTPS
-        // Cloudflare selalu mengirimkan header X-Forwarded-Proto
-        if (Request::header('X-Forwarded-Proto') === 'https') {
-            URL::forceScheme('https');
-        }
+        // // 1. Cek apakah request datang dari proxy/cloudflare dengan HTTPS
+        // // Cloudflare selalu mengirimkan header X-Forwarded-Proto
+        // if (Request::header('X-Forwarded-Proto') === 'https') {
+        //     URL::forceScheme('https');
+        // }
         
-        // 2. (Opsional) Jika Anda menggunakan Laravel 11, pastikan internal proxy di-trust 
-        // agar header di atas terbaca dengan benar oleh Laravel
-        if (isset($this->app['request'])) {
-            $this->app['request']->setTrustedProxies(
-                ['127.0.0.1', '::1'], // IP localhost/cloudflared tunnel
-                \Symfony\Component\HttpFoundation\Request::HEADER_X_FORWARDED_FOR |
-                \Symfony\Component\HttpFoundation\Request::HEADER_X_FORWARDED_PROTO |
-                \Symfony\Component\HttpFoundation\Request::HEADER_X_FORWARDED_HOST
-            );
+        // // 2. (Opsional) Jika Anda menggunakan Laravel 11, pastikan internal proxy di-trust 
+        // // agar header di atas terbaca dengan benar oleh Laravel
+        // if (isset($this->app['request'])) {
+        //     $this->app['request']->setTrustedProxies(
+        //         ['127.0.0.1', '::1'], // IP localhost/cloudflared tunnel
+        //         \Symfony\Component\HttpFoundation\Request::HEADER_X_FORWARDED_FOR |
+        //         \Symfony\Component\HttpFoundation\Request::HEADER_X_FORWARDED_PROTO |
+        //         \Symfony\Component\HttpFoundation\Request::HEADER_X_FORWARDED_HOST
+        //     );
+        // }
+        // Cek apakah request datang dari Cloudflare (https)
+        if (request()->header('X-Forwarded-Proto') === 'https') {
+            
+            // 1. Paksa Laravel menggunakan URL https
+            URL::forceScheme('https');
+            
+            // 2. INI KUNCI UNTUK LIVEWIRE 3: 
+            // Paksa server state menjadi HTTPS 'on' agar Livewire tidak menggunakan http://
+            request()->server->set('HTTPS', 'on'); 
         }
     }
 
